acf39c9e0456fcce08717806a75d9d5c9878b43b
Frayman <Frayman@DESKTOP-4U42C65>
2025-01-12 15:14:40 +0200

    update error message

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 7000;
-const RENDER_API_KEY = process.env.API_KEY; 
-
+const API_KEY = process.env.API_KEY;
 
 app.get('/', (req, res) => {
     res.send('Hello from Render!');
@@ -12,25 +11,28 @@ app.get('/', (req, res) => {
 
 app.get('/services', async (req, res) => {
     try {
-        if (!RENDER_API_KEY) {
-            throw new Error("RENDER_API_KEY is missing in .env file");
+        if (!API_KEY) {
+            console.error("API_KEY is missing in .env file"); 
+            return res.status(500).send('An internal error occurred.'); 
         }
+
         const response = await axios.get('https://api.render.com/v1/services', {
-            headers: {
-                Authorization: `Bearer ${RENDER_API_KEY}`
-            },
+            headers: { Authorization: `Bearer ${API_KEY}` },
         });
+
         res.json(response.data);
     } catch (error) {
-        console.error("Error details:", error); // הדפסת שגיאה מלאה עם פרטים
+        console.error("Error details:", error);
         if (error.response) {
-            res.status(error.response.status).send(`Error fetching services: ${error.response.status} - ${error.response.data}`);
+            console.error("Response data:", error.response.data);
+            console.error("Response status:", error.response.status);
+            return res.status(500).send('An internal error occurred.'); 
         } else if (error.request) {
             console.error("Request error:", error.request);
-            res.status(500).send('Error fetching services: No response from server');
+            return res.status(500).send('Failed to connect to Render API.'); 
         } else {
             console.error('Error message:', error.message);
-            res.status(500).send(`Error fetching services: ${error.message}`);
+            return res.status(500).send('An internal error occurred.'); 
         }
     }
 });
